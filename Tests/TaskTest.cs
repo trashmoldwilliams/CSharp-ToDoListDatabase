56dc52e339fee7c4e5fe661bd54eaaa9d597c876
Chris Kuiper and Will Johnson <chris.kuiper@gmail.com and trashmoldwilliams@gmail.com>
2016-02-22 10:59:16 -0800

    create initial files

---
+++ Tests/TaskTest.cs
@@ -0,0 +1,21 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ToDoList
+{
+  public class ToDoTest : IDisposable
+  {
+    public ToDoTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=todo_test;Integrated Security=SSPI;";
+    }
+
+    public void Dispose()
+    {
+      Task.DeleteAll();
+    }
+  }
+}
